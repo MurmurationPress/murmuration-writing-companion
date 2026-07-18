b963376a8799992782aa53883c9988e4776f239c
tedwarddick <108003581+tedwarddick@users.noreply.github.com>
2026-07-18 08:37:18 +0100

    Install manuscript reconciliation workflow

--- src/entry.ts
+++ src/entry.ts
@@ -1,9 +1,11 @@
 import MurmurationWritingCompanionPlugin from "./main";
 import { installManuscriptPreparationCommands } from "./manuscript/ManuscriptPreparationCommands";
+import { installManuscriptReconciliationCommands } from "./manuscript/ManuscriptReconciliationCommands";
 
 export default class MurmurationWritingCompanionEntry extends MurmurationWritingCompanionPlugin {
   async onload() {
     await super.onload();
     installManuscriptPreparationCommands(this);
+    installManuscriptReconciliationCommands(this);
   }
 }
